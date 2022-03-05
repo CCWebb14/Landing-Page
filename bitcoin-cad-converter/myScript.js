3e75f0c9e49ed4bfcecc30c11af214978877e9a1
Cooper Webb <CCWebb14@gmail.com>
2022-03-04 17:32:09 -0800

    Live updating based on input

--- bitcoin-cad-converter/myScript.js
+++ bitcoin-cad-converter/myScript.js
@@ -1,22 +1,47 @@
 
 const res = document.getElementById('res');
 
-const entry_bar_content = document.getElementById('entry-bar-content');
-entry_bar_content.classList.add('entry-bar-content-animation');
+const input = document.getElementById('entry-bar-content');
+input.classList.add('entry-bar-content-animation');
 
-// fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=cad&ids=bitcoin')
-fetch('https://api.coingecko.com/api/v3/coins/bitcoin')
-    .then(function(response) {
-    console.log("IT WORKED");
-        return response.json();
-    })
-    // .catch(function(err) {
-    // // Error :(
-    //     console.log("API_ERROR");
-    // })
-    .then(function(response) {
-        console.log(response.market_data.current_price.cad);
-        res.innerText = "$ " + response.market_data.current_price.cad;
-        // console.log(response.name);
-    });
+input.addEventListener('input', inputResponse);
+
+function inputResponse(e){
+    input.classList.remove('entry-bar-content-animation');
+
+    factor = input.value;
+    console.log(factor);
+
+    if (isNaN(factor)){
+        res.innerText = "NaN";
+    }
+    if (!isNaN(factor)){
+        getPrice(parseFloat(factor));
+    }
+
+
+    // getPrice(parseFloat(factor));
+
+
+    
+}
+
+function getPrice(factor){
+    fetch('https://api.coingecko.com/api/v3/coins/bitcoin')
+        .then(function(response) {
+        console.log("IT WORKED");
+            return response.json();
+        })
+        .then(function(response) {
+            console.log(response.market_data.current_price.cad);
+            price = parseFloat(response.market_data.current_price.cad);
+            result = (price * factor).toFixed(2);
+            if (isNaN(result)){
+                res.innerText = ""
+            }
+            else{
+                res.innerText = "$ " + result;
+            }
+        });
+}
     
