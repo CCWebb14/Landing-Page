044d7cd1331dc88cd619f5a1ae2d73ec66ab922c
Cooper Webb <CCWebb14@gmail.com>
2022-03-04 16:48:17 -0800

    On click event removes entry-bar placeholder text, changed animation to manipulated class

--- bitcoin-cad-converter/myScript.js
+++ bitcoin-cad-converter/myScript.js
@@ -1,6 +1,9 @@
 
 const res = document.getElementById('res');
 
+const entry_bar_content = document.getElementById('entry-bar-content');
+entry_bar_content.classList.add('entry-bar-content-animation');
+
 // fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=cad&ids=bitcoin')
 fetch('https://api.coingecko.com/api/v3/coins/bitcoin')
     .then(function(response) {
